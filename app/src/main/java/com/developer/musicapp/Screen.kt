baad5c1e364a95f1574ee76b1d989d24d168e0c7
Martin Bebey <37882687+martinbebey@users.noreply.github.com>
2024-09-13 18:17:27 -0400

    added drawer items

---
+++ app/src/main/java/com/developer/musicapp/Screen.kt
@@ -0,0 +1,33 @@
+package com.developer.musicapp
+
+import androidx.annotation.DrawableRes
+
+sealed class Screen(
+    val title: String,
+    val route: String
+) {
+
+    sealed class DrawerScreen(
+        val dTitle:String,
+        val dRoute: String,
+        @DrawableRes val icon: Int
+    ): Screen(dTitle, dRoute){
+        object Account: DrawerScreen(
+            dTitle = "Account",
+            dRoute = "account",
+            R.drawable.ic_account
+        )
+
+        object Subscription: DrawerScreen(
+            dTitle = "Subscription",
+            dRoute = "subscribe",
+            R.drawable.ic_subscribe
+        )
+
+        object AddAccount: DrawerScreen(
+            dTitle = "Add Account",
+            dRoute = "add_account",
+            R.drawable.baseline_person_add_alt_1_24
+        )
+    }
+}

c6841cb8516703f61c969f3adf076d3354c31afc
Martin Bebey <37882687+martinbebey@users.noreply.github.com>
2024-09-13 21:58:24 -0400

    set up modal sheet layout

--- app/src/main/java/com/developer/musicapp/ui/theme/MainView.kt
+++ app/src/main/java/com/developer/musicapp/ui/theme/MainView.kt
@@ -3,15 +3,22 @@ package com.developer.musicapp.ui.theme
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -19,9 +26,12 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Scaffold
-import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.primarySurface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -33,6 +43,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
@@ -46,6 +57,7 @@ import com.developer.musicapp.Browse
 import com.developer.musicapp.Home
 import com.developer.musicapp.Library
 import com.developer.musicapp.MainViewModel
+import com.developer.musicapp.R
 import com.developer.musicapp.Screen
 import com.developer.musicapp.Subscription
 import com.developer.musicapp.screensInBottom
@@ -53,14 +65,16 @@ import com.developer.musicapp.screensInDrawer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun MainView(){
 
     val scaffoldState: ScaffoldState = rememberScaffoldState()
     val scope: CoroutineScope = rememberCoroutineScope()
     val viewModel: MainViewModel = viewModel()
+    val isSheetFullScreen by remember{ mutableStateOf(false) }
 
+    val modifier = if(isSheetFullScreen) Modifier.fillMaxSize() else Modifier.fillMaxWidth()
     //allow us to find out on which route/screen we currently are
     val controller: NavController = rememberNavController()
     val navBackStackEntry by controller.currentBackStackEntryAsState()
@@ -78,6 +92,13 @@ fun MainView(){
 
         mutableStateOf(currentScreen.title)
     }
+
+    val modalSheetState = androidx.compose.material.rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        confirmValueChange = { it != ModalBottomSheetValue.HalfExpanded}
+    )
+
+    val roundedCornerRadius = if(isSheetFullScreen) 0.dp else 12.dp
 
     val bottomBar:  @Composable () -> Unit = {
         if(currentScreen is Screen.DrawerScreen || currentScreen == Screen.BottomScreen.Home){
@@ -104,52 +125,66 @@ fun MainView(){
             }
         }
     }
-    
-    Scaffold(
-        bottomBar = bottomBar,
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = title.value)
-                },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        //opening the drawer
+
+    ModalBottomSheetLayout(
+        sheetState = modalSheetState,
+        sheetShape = RoundedCornerShape(topStart = roundedCornerRadius, topEnd = roundedCornerRadius),
+        sheetContent = {
+            MoreBottomSheet(modifier = modifier)
+        }) {
+        Scaffold(
+            bottomBar = bottomBar,
+            topBar = {
+                TopAppBar(title = { Text(title.value) },
+                    actions = {
+                        IconButton(
+                            onClick = {
+                                scope.launch {
+                                    if(modalSheetState.isVisible)
+                                        modalSheetState.hide()
+                                    else
+                                        modalSheetState.show()
+                                }
+                            }
+                        ) {
+                            Icon(imageVector = Icons.Default.MoreVert, contentDescription = null)
+                        }
+                    },
+                    navigationIcon = { IconButton(onClick = {
+                        // Open the drawer
                         scope.launch {
                             scaffoldState.drawerState.open()
                         }
-
                     }) {
-                       Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu")
-                    }
-                }
-            )
-        },
-        scaffoldState = scaffoldState,
-        drawerContent = {
-            LazyColumn(modifier = Modifier.padding(16.dp)) {
-                items(screensInDrawer){
-                    drawerItem ->
-                    DrawerItem(selected = currentRoute == drawerItem.dRoute, item = drawerItem) {
-                        //close drawer on click
-                        scope.launch {
-                            scaffoldState.drawerState.close()
-                        }
-
-                        if(drawerItem.dRoute == "add_account"){
-                            dialogOpen.value = true
-                        }
-                        else{
-                            controller.navigate(drawerItem.dRoute)
-                            title.value = drawerItem.title
+                        Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu" )
+                    }}
+                )
+            },scaffoldState = scaffoldState,
+            drawerContent = {
+                LazyColumn(Modifier.padding(16.dp)){
+                    items(screensInDrawer){
+                            item ->
+                        DrawerItem(selected = currentRoute == item.dRoute, item = item) {
+                            scope.launch {
+                                scaffoldState.drawerState.close()
+                            }
+                            if(item.dRoute == "add_account"){
+                                dialogOpen.value= true
+                            }else{
+                                controller.navigate(item.dRoute)
+                                title.value = item.dTitle
+                            }
                         }
                     }
                 }
             }
+
+        ) {
+            Navigation(navController = controller, viewModel = viewModel, pd = it)
+
+            AccountDialog(dialogOpen = dialogOpen)
+
         }
-    ) {
-        Navigation(navController = controller, viewModel = viewModel, pd = it)
-        AccountDialog(dialogOpen = dialogOpen)
     }
 }
 
@@ -175,8 +210,50 @@ fun DrawerItem(
         )
         Text(
             text = item.dTitle,
-            style = MaterialTheme.typography.headlineSmall,
+            style = MaterialTheme.typography.h5,
         )
+    }
+}
+
+@Composable
+fun MoreBottomSheet(modifier: Modifier){
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .background(
+                MaterialTheme.colors.primarySurface
+            )
+    ){
+        Column(modifier = modifier.padding(16.dp), verticalArrangement = Arrangement.SpaceBetween){
+            Row(modifier = modifier.padding(16.dp)){
+                Icon(modifier = Modifier.padding(end = 8.dp),
+                    painter =  painterResource(id = R.drawable.baseline_settings_24),
+                    contentDescription = "Settings")
+                Text(text = "Settings", fontSize = 20.sp, color = Color.White)
+            }
+            Row(modifier = modifier.padding(16.dp)) {
+                androidx.compose.material.Icon(
+                    modifier = Modifier.padding(end = 8.dp),
+                    painter = painterResource(id = R.drawable.ic_baseline_share_24),
+                    contentDescription = "Share"
+                )
+
+                androidx.compose.material.Text(
+                    text = "Share",
+                    fontSize = 20.sp,
+                    color = Color.White
+                )
+            }
+            Row(modifier = modifier.padding(16.dp)) {
+                androidx.compose.material.Icon(
+                    modifier = Modifier.padding(end = 8.dp),
+                    painter = painterResource(id = R.drawable.ic_help_green),
+                    contentDescription = "Help"
+                )
+                androidx.compose.material.Text(text = "Help", fontSize = 20.sp, color = Color.White)
+            }
+        }
     }
 }
 
